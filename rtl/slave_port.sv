43dfe10ec7d0ed1c7ba14b81995f4d650a72dfd9
SupunDK <supun2110@gmail.com>
2023-10-03 21:20:46 +0530

    made the output decoder of the slave port

--- rtl/slave_port.sv
+++ rtl/slave_port.sv
@@ -21,12 +21,17 @@ module slave_port #(
 // definition of states
 localparam NUM_STATES = 6;
 localparam STATE_N_BITS = $clog2(NUM_STATES);
+
 // RX_ADDR_P1: receive address part 1 (first 4 bits), RX_ADDR_P2: receive address part 2 (last 8 bits), RS: receiveing from slave, WS: writing to slave, TX: transmitting serial data, RX_DATA: receiving serial data
 enum logic [STATE_N_BITS-1:0] {RX_ADDR_P1, RX_ADDR_P2, RS, TX, RX_DATA, WS} state, next_state;
 
 // counter to keep track of the number of bits received/transmitted
 localparam N_BITS = $clog2(ADDR_W);
 logic [N_BITS-1:0] count;
+
+// internal registers to hold the address and the data
+logic [ADDR_W-1:0] internal_addr;
+logic [DATA_W-1:0] internal_data;
 
 always_comb begin : NEXT_STATE_DECODER
   unique case (state)
@@ -39,8 +44,79 @@ always_comb begin : NEXT_STATE_DECODER
   endcase
 end
 
+// state sequencer with active low reset
 always_ff(@posedge in_clk or negedge reset_n) begin : STATE_SEQUENCER
   state = reset_n == 0 ? RX_ADDR_P1 : next_state;
+end
+
+assign out_clk = in_clk;
+assign ser_rdata = internal_data[0];
+assign out_split_en = in_split_en; // TODO: think about this when implementing split transactions
+
+always_ff(@posedge in_clk or negedge reset_n) begin : OUTPUT_DECODER
+  if (reset_n == 0) count <= N_BITS'd0;
+  else unique case (state)
+    RX_ADDR_P1: begin
+                  par_out_valid_ready <= 1'd0;
+                  ser_out_valid_ready <= 1'd1;
+
+                  if (ss == 1 && ser_in_valid_ready == 1) begin
+                    internal_addr <= internal_addr >> 1;
+                    internal_addr[ADDR_W-1] <= in_addr;
+                    count <= count + 1;
+                  end
+                end
+    RX_DATA:    begin
+                  par_out_valid_ready <= 1'd0;
+                  ser_out_valid_ready <= 1'd1;
+
+                  if (ss == 1 && ser_in_valid_ready == 1) begin
+                    internal_addr <= internal_addr >> 1;
+                    internal_addr[ADDR_W-1] <= in_addr;
+
+                    internal_data <= internal_data >> 1;
+                    internal_data[DATA_W-1] <= ser_wdata;
+                    count <= count + 1;
+                  end
+                end
+    RX_ADDR_P2: begin
+                  par_out_valid_ready <= 1'd0;
+                  ser_out_valid_ready <= 1'd1;
+
+                  if (ss == 1 && ser_in_valid_ready == 1) begin
+                    internal_addr <= internal_addr >> 1;
+                    internal_addr[ADDR_W-1] <= in_addr;
+                    count <= count + 1;
+                  end
+                end
+    TX:         begin
+                  par_out_valid_ready <= 1'd0;
+                  ser_out_valid_ready <= 1'd1;
+
+                  if (ss == 1 && ser_in_valid_ready == 1) begin
+                    internal_data <= internal_data >> 1;
+                    count <= count + 1;
+                  end
+                end
+    WS:         begin
+                  ser_out_valid_ready <= 1'd0;
+                  par_out_valid_ready <= 1'd1;
+                  out_write <= 1'd1;
+
+                  par_wdata <= internal_data;
+                  out_addr <= internal_addr;
+                end
+    RS:         begin
+                  ser_out_valid_ready <= 1'd0;
+                  par_out_valid_ready <= 1'd1;
+                  out_write <= 1'd0;
+
+                  out_addr <= internal_addr;
+                  internal_data <= par_rdata;
+
+                  count <= 0;
+                end
+  endcase
 end
 
 endmodule
