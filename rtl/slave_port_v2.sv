2657e592ecd86dacc6c2185194537d6cb10cb511
SupunDK <supun2110@gmail.com>
2023-12-02 21:11:08 +0530

    changed the DATA_IN state decoder

--- rtl/slave_port_v2.sv
+++ rtl/slave_port_v2.sv
@@ -30,7 +30,7 @@ always_comb begin : NEXT_STATE_DECODER
     unique0 case (state)
         IDLE: next_state = ( ( master_valid == 1 ) ? ADDR_IN : IDLE );
         ADDR_IN: next_state = ( (counter < ADDR_WIDTH) ? ( (port_ready == 1 && master_valid == 1 ) ? ADDR_IN : IDLE ) : ( (mode == 1) ? DATA_IN : READ ) );
-        DATA_IN: next_state = ( (counter < ADDR_WIDTH+DATA_WIDTH) ? ( ( port_ready == 1 && master_valid == 1 ) ? DATA_IN : IDLE ) : WRITE );
+        DATA_IN: next_state = ( (counter < ADDR_WIDTH+DATA_WIDTH-1) ? ( ( port_ready == 1 && master_valid == 1 ) ? DATA_IN : IDLE ) : WRITE );
         WRITE: next_state = IDLE;
         READ: next_state = SEND_PREL;
         SEND_PREL: next_state = SEND;
