003b660cad72d58a4cb79ddce1ea9e21a385cf62
SupunDK <supun2110@gmail.com>
2023-12-02 21:18:23 +0530

    revert DATA_IN back and changed ADDR_IN

--- rtl/slave_port_v2.sv
+++ rtl/slave_port_v2.sv
@@ -29,8 +29,8 @@ assign slave_valid = port_valid;
 always_comb begin : NEXT_STATE_DECODER
     unique0 case (state)
         IDLE: next_state = ( ( master_valid == 1 ) ? ADDR_IN : IDLE );
-        ADDR_IN: next_state = ( (counter < ADDR_WIDTH) ? ( (port_ready == 1 && master_valid == 1 ) ? ADDR_IN : IDLE ) : ( (mode == 1) ? DATA_IN : READ ) );
-        DATA_IN: next_state = ( (counter < ADDR_WIDTH+DATA_WIDTH-2) ? ( ( port_ready == 1 && master_valid == 1 ) ? DATA_IN : IDLE ) : WRITE );
+        ADDR_IN: next_state = ( (counter < ADDR_WIDTH-1) ? ( (port_ready == 1 && master_valid == 1 ) ? ADDR_IN : IDLE ) : ( (mode == 1) ? DATA_IN : READ ) );
+        DATA_IN: next_state = ( (counter < ADDR_WIDTH+DATA_WIDTH) ? ( ( port_ready == 1 && master_valid == 1 ) ? DATA_IN : IDLE ) : WRITE );
         WRITE: next_state = IDLE;
         READ: next_state = SEND_PREL;
         SEND_PREL: next_state = SEND;
