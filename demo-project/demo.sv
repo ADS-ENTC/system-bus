2430b8e24153e1d1dcd16a4a62e6d027a1c52cf4
KCSAbeywickrama <abeywickramakcs.19@uom.lk>
2023-12-05 17:27:45 +0530

    init bcd instance with bcd2seg

--- demo-project/demo.sv
+++ demo-project/demo.sv
@@ -1,2 +1,17 @@
-module demo();
+
+module demo (
+    input logic clk,       
+    input logic [3:0] keys,
+    input logic [17:0] sws,
+    output logic [6:0] hex0,
+    output logic [6:0] hex1,
+    output logic [6:0] hex2,
+    output logic m1,
+    output logic m2
+);
+  // Module implementation goes here
+bcd7seg seg0(.bcd(keys), .seg(hex0));
+
+
+  
 endmodule
