c2f163c336851bd52c6402aaf58d9ef718c629e2
SupunDK <supun2110@gmail.com>
2023-12-06 21:19:21 +0530

    made a rd_ptr and wr_ptr fifo

---
+++ rtl/fifo_v2.sv
@@ -0,0 +1,37 @@
+module FIFO #(parameter WIDTH = 32, parameter DEPTH = 16) (
+    input logic clk, 
+    input logic rstn, 
+    input logic [WIDTH-1:0] data_in, 
+    input logic enq, 
+    input logic deq, 
+    output logic [WIDTH-1:0] data_out,
+    output logic empty,
+    output logic full
+);
+
+logic [DEPTH-1:0][WIDTH-1:0] memory; //packed array
+logic [$clog2(DEPTH):0] rd_ptr, wr_ptr;
+
+assign data_out = memory[rd_ptr];
+assign empty = (rd_ptr == wr_ptr);
+assign full = (rd_ptr == wr_ptr + 1);
+
+always_ff @(posedge clk or negedge rstn) begin
+    if(!rstn) begin
+        rd_ptr <= 0;
+        wr_ptr <= 0;
+    end
+
+    else begin
+        if (enq) begin
+            memory[wr_ptr] <= data_in;
+            wr_ptr <= wr_ptr + 1;
+        end
+
+        if (deq) begin
+            rd_ptr <= rd_ptr + 1;
+        end
+    end
+end
+
+endmodule
